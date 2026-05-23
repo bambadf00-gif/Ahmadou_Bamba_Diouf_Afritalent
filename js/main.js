3f8ce81437b11de3cec000c92aa6824a77851079
Bamba <bamba.df00@gmail.com>
2026-05-23 15:35:07 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -85,15 +85,107 @@ for (let prest of TousNbr) {
 const sectionFondu = document.querySelectorAll("section");
 
 const observateurSection = new IntersectionObserver((courSection) => {
-  for (let sec of courSection){
-    if(sec.isIntersecting){
+  for (let sec of courSection) {
+    if (sec.isIntersecting) {
       sec.target.classList.add("visbleSection");
-    }else{
+    } else {
       sec.target.classList.remove("visbleSection");
     }
   }
+});
+
+for (let sectionVue of sectionFondu) {
+  observateurSection.observe(sectionVue);
+}
+
+/*=====================================================
+filtrage dynamique des freelances 
+===================================================== */
+const filtre = document.querySelectorAll(".btnFiltre");
+const articles = document.querySelectorAll("article");
+
+for (let art of articles) {
+  art.classList.add("visi"); // affichage de tout les cartpar  defaut 
+}
+
+for (let btn of filtre) {
+  btn.addEventListener("click", () => {
+    for (let art of articles) {
+      art.classList.add("visi");
+      if (btn.dataset.category === "tous") {
+        art.classList.add("visi");
+      } else if (btn.dataset.category === art.dataset.category) {
+        art.classList.add("visi");
+      } else {
+        art.classList.remove("visi");
+      }
+    }
+  })
+}
+
+
+// =================================================
+// validation du formulaire de contac
+//==================================================
+
+//Sélection des éléments
+const formulaire = document.querySelector(".formulaire");
+const nomUser = document.querySelector("#nomUser");
+const prenomUser = document.querySelector("#prenomUser");
+const mailUser = document.querySelector("#mailUser");
+const messageUser = document.querySelector("#message");
+
+//Sélection des paragraphes de messages d'erreur CSS
+const erreurNom = document.querySelector(".erreurNom");
+const erreurPrenom = document.querySelector(".erreurPrenom");
+const erreurMail = document.querySelector(".erreurMail");
+const erreurMessage = document.querySelector(".erreurMessage");
+
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const pupPupMessage = document.querySelector(".pupPupMessage");
+const btnPuppup = document.querySelector(".pupPupMessage");
+
+
+formulaire.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (nomUser.value.trim().length < 2) {
+    erreurNom.classList.add("erreurVisible");
+  } else {
+    erreurNom.classList.remove("erreurVisible");
+  }
+
+  if (prenomUser.value.trim().length < 3) {
+    erreurPrenom.classList.add("erreurVisible");
+  } else {
+    erreurPrenom.classList.remove("erreurVisible");
+  }
+
+  if (messageUser.value.trim().length < 20) {
+    erreurMessage.classList.add("erreurVisible");
+  } else {
+    erreurMessage.classList.remove("erreurVisible");
+  }
+
+  if (regex.test(mailUser.value.trim())) {
+    erreurMail.classList.remove("erreurVisible");
+  } else {
+    erreurMail.classList.add("erreurVisible");
+  }
+
+  if (
+    nomUser.value.trim().length > 2 &&
+    prenomUser.value.trim().length > 3 &&
+    messageUser.value.trim().length > 20 &&
+    regex.test(mailUser.value.trim())
+  ) {
+    pupPupMessage.classList.add("messageVue");
+  } else {
+    pupPupMessage.classList.remove("messageVue");
+  }
 })
 
-for (let sectionVue of  sectionFondu){
-  observateurSection.observe(sectionVue);
-} 
+btnPuppup.addEventListener("click", () => {
+  pupPupMessage.classList.add("messageVueFerme");
+})
